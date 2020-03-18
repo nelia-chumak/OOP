42c10ef6659b5e17c17560964e79aebda88aab53
neonilakunyk <n.kunyk-2025@kpi.ua>
2020-03-18 18:34:50 +0200

    Добавлена 3я лабораторная

---
+++ ЛР3 Индексаторы и свойства C-sharp/Class1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ЛР3_Индексаторы_и_свойства_C_sharp
+{
+    public class Class1
+    {
+        public static int row, col;
+        public int rows, cols;
+        public int start;
+        public bool error;
+        private int[,] arr = new int[row, col];
+        private int mult;
+
+        public Class1(int s, int r, int c)
+        {
+            start = s;
+            arr = new int[r, c];
+            row = r;
+            col = c;
+            error = false;
+            rows = r;
+            cols = c;
+        }
+        public float this[int index]
+        {
+            get
+            {
+                index = index - start;
+                if (index >= 0 && index < cols * rows)
+                {
+                    error = false;
+                    float sum = 0;
+                    for (int i = 0; i < cols; i++)
+                    {
+                        sum += arr[index, i];
+                    }
+                    return sum / cols;
+                }
+                else
+                {
+                    error = true;
+                    return 0;
+                }
+            }
+        }
+        public int this[int index1, int index2]
+        {
+            set
+            {
+                if (index1 >= 0 && index1 < rows && index2 >= 0 && index2 < cols)
+                {
+                    error = false;
+                    arr[index1, index2] = value;
+                }
+                else
+                {
+                    error = true;
+                }
+            }
+            get
+            {
+                if (index1 >= 0 && index1 < rows && index2 >= 0 && index2 < cols)
+                {
+                    error = false;
+                    return arr[index1, index2];
+                }
+                else
+                {
+                    error = true;
+                    return 0;
+                }
+            }
+        }
+        public int Mult
+        {
+            get
+            {
+                mult = 1;
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        mult *= arr[i, j];
+                    }
+                }
+                return mult;
+            }
+        }
+        public void Print()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write("{0} ", arr[i, j]);
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ ЛР3 Индексаторы и свойства C-sharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ЛР3_Индексаторы_и_свойства_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 array = new Class1(1, 2, 3);
+            for (int i = 0; i <= array.rows; i++)
+            {
+                for (int j = 0; j <= array.cols; j++)
+                {
+                    array[i, j] = i * array.cols + j + 1;
+                }
+            }
+            array.Print();
+            Console.WriteLine(array[1]);
+            Console.WriteLine(array[2]);
+            Console.WriteLine(array.Mult);
+        }
+    }
+}
